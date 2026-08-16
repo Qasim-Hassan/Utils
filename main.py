d2314808225a455bba043d3a5730e01afb298c64
Qasim Hassan <qasimhassan1947@gmail.com>
2026-08-16 09:58:09 +0000

    writing util for removing background from images

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from rembg import remove
 from PIL import Image
 
+input_path = 'input.jpg'
+output_path = 'output.png'
+
+input_image = Image.open(input_path)
+
+# Remove the background
+output_image = remove(input_image)
+
+# Save the result
+output_image.save(output_path)
