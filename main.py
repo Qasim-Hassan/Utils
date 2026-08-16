fad0354fb50449e92b4e9c3c2338305b76b90958
Qasim Hassan <qasimhassan1947@gmail.com>
2026-08-16 10:07:56 +0000

    writing util for removing background from images

--- main.py
+++ main.py
@@ -7,7 +7,13 @@ output_path = 'output.png'
 input_image = Image.open(input_path)
 
 # Remove the background
-output_image = remove(input_image)
+output_image = remove(
+    input_image,
+    alpha_matting=True,
+    alpha_matting_foreground_threshold=240,
+    alpha_matting_background_threshold=10,
+    alpha_matting_erode_size=10
+)
 
 # Save the result
 output_image.save(output_path)
